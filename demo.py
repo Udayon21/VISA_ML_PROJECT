391929d2be953f0aa12c1f52dae1385db433d8ae
Udayon21 <pauludayon@gmail.com>
2025-04-18 23:12:28 +0530

    second commit

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from VISA_ML_PROJECT.logger import logging
+from VISA_ML_PROJECT.exception import visaException
+import sys
+#logging.info("Welcome to our custom log")
 
-logging.info("Welcome to our custom log")
+try:
+    a=2/0
+except Exception as e:
+    raise  visaException(e,sys)   
